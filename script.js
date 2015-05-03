7d0f74e078960fe80ee8fb9ddfcfc08efe8ea7b6
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2015-05-04 00:45:57 +0900

    replace only text

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.body.innerHTML = document.body.innerHTML.replace(/Bug/g, 'Dragon').replace(/bug/g, 'dragon').replace(/バグ/g, 'ドラゴン').replace(/ばぐ/g, 'どらごん');
+// rndomhack https://gist.github.com/rndomhack/52ce66a99587deff8802
+(function() {
+    var list, toDragon;
+    list = [["bug", "dragon"], ["Bug", "Dragon"], ["バグ", "ドラゴン"], ["ばぐ", "どらごん"], ["ﾊﾞｸﾞ", "ﾄﾞﾗｺﾞﾝ"]];
+    (toDragon = function(node) {
+        if (node.nodeValue !== null) {
+            [].forEach.call(list, function(item) {
+                node.nodeValue = node.nodeValue.split(item[0]).join(item[1]);
+            });
+        }
+        [].forEach.call(node.childNodes, function(child) {
+            toDragon(child);
+        });
+    })(document.body);
+})();
